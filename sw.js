15d341f48a772aa2030b858d3e86446478fb7679
Jatisampurna <272405221+seesalam@users.noreply.github.com>
2026-04-11 13:48:31 +0700

    launch

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'webview-cache-v1';
+const urlsToCache = ['/'];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetching data
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
